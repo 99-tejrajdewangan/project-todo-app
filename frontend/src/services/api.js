c4b79048c09f8464f2a92e2f474425fc14cc30e1
Tejraj Dewangan <tejrajdewangan99@gmail.com>
2026-04-23 04:30:55 +0530

    URI update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://projecttodoapp-sepia.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://projecttodoraj.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
